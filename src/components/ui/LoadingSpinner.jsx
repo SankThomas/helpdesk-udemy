789ab6ef9115c92d10a45b525f636dc62f51e88a
SankThomas <thomassankara74@gmail.com>
2026-02-27 12:29:24 +0300

    loading spinner component

---
+++ src/components/ui/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+export const LoadingSpinner = () => {
+  return (
+    <div className="flex h-64 items-center justify-center">
+      <div className="border-primary-600 size-8 animate-spin rounded-full border-b-2"></div>
+    </div>
+  );
+};
