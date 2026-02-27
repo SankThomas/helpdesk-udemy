f0ad0fdec0d2911412ec08114a93d5af207eadc0
SankThomas <thomassankara74@gmail.com>
2026-02-27 12:28:05 +0300

    add providers

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
+import App from "./App.jsx";
+import "./index.css";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ConvexProvider client={convex}>
+        <BrowserRouter>
+          <App />
+          <Toaster richColors />
+        </BrowserRouter>
+      </ConvexProvider>
+    </ClerkProvider>
+  </StrictMode>,
+);
