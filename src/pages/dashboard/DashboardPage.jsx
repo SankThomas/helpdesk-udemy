cf64666d9d079d64dd17aa84b936dec5bd43eeb8
SankThomas <thomassankara74@gmail.com>
2026-02-28 15:51:52 +0300

    render dashboard page based on user role

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { useCurrentUser } from "../../hooks/useCurrentUser";
+import { LoadingSpinner } from "../../components/ui/LoadingSpinner";
+import { AdminDashboard } from "../../components/dashboard/AdminDashboard";
+import { AgentDashboard } from "../../components/dashboard/AgentDashboard";
+import { UserDashboard } from "../../components/dashboard/UserDashboard";
 
 export const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const { currentUser } = useCurrentUser();
+
+  if (!currentUser) {
+    return <LoadingSpinner />;
+  }
+
+  switch (currentUser.role) {
+    case "admin":
+      return <AdminDashboard user={currentUser} />;
+    case "agent":
+      return <AgentDashboard user={currentUser} />;
+    case "user":
+      return <UserDashboard user={currentUser} />;
+  }
 };
