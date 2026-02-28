96338a9b6f90f4d5301750ed3ee4ad2eba031df7
SankThomas <thomassankara74@gmail.com>
2026-02-28 15:52:02 +0300

    build custom error page

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/ui/Button";
 
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="mx-auto flex min-h-96 max-w-4xl flex-col items-center justify-center space-y-4 text-center">
+      <h1>Oops, this page no longer exists</h1>
+      <p>
+        Seems like you have found a page that is no longer on the website.
+        Aplogies. Browse oother working pages.
+      </p>
+
+      <Link to="/dashboard">
+        <Button variant="outline" size="sm">
+          &larr; Back to dashboard
+        </Button>
+      </Link>
+    </div>
+  );
 };
